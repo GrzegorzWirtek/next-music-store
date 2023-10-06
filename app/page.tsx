6e4d3d4c8db78cbfff060ecd6d5b461233570c7e
GrzegorzWirtek <grzegorz.wirtek@wp.pl>
2023-10-06 22:48:14 +0200

    move home content to one component to enable responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,12 @@
 import HomeHeader from '../components/HomeHeader';
 import getProducts from '@/utils/getProducts';
-import Product from '@/components/Product';
-import { Product as ProductProps } from '@/utils/types';
 
 export default async function Home() {
 	const products = await getProducts();
-	const mainProductIndex = products.findIndex(
-		(product: ProductProps) => product.main === true,
-	);
-	const mainProduct = products.splice(mainProductIndex, 1);
 
 	return (
 		<main className='max-w-7xl m-auto'>
-			<HomeHeader mainProduct={mainProduct[0]} />
-			<section className='flex flex-wrap gap-10 justify-center mt-24 mb-60'>
-				{products.map((product: ProductProps) => (
-					<Product key={product._id} product={product} />
-				))}
-			</section>
+			<HomeHeader products={products} />
 		</main>
 	);
 }
--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -1,21 +1,26 @@
 import Image from 'next/image';
 import { Playfair_Display } from 'next/font/google';
 import Button from './Button';
-import { Product } from '@/utils/types';
+import Product from '@/components/Product';
+import { Product as ProductProps } from '@/utils/types';
 
 const plaifairDisplay = Playfair_Display({
 	subsets: ['latin'],
 	weight: ['400'],
 });
 
-export default function HomeHeader({ mainProduct }: { mainProduct: Product }) {
-	const { title, price, images } = mainProduct;
+export default function HomeHeader({ products }: { products: ProductProps[] }) {
+	const mainProductIndex = products.findIndex(
+		(product: ProductProps) => product.main === true,
+	);
+	const mainProduct = products.splice(mainProductIndex, 1)[0];
+
 	const baseImgUrl = process.env.UPLOADTHING_BASE_URL;
 	return (
-		<section className='px-10 flex flex-row grow flex-wrap justify-center'>
-			<div className='flex flex-col basis-1/2 items-end justify-center'>
+		<section className='sm:px-2 md:px-10 flex flex-row grow flex-wrap justify-center gap-4'>
+			<div className='flex flex-col basis-[340px] sm:basis-[460px] xl:basis-[570px] my-10 lg:my-0 items-end justify-center'>
 				<h2
-					className={`${plaifairDisplay.className} mb-3 text-[4.7vw] xl:text-6xl text-right text-[var(--blue)] leading-none`}>
+					className={`${plaifairDisplay.className} mb-3 text-4xl sm:text-5xl md:text-5xl xl:text-6xl text-right text-[var(--blue)] leading-none`}>
 					If you want your music to be the best, you need the best instruments.
 				</h2>
 				<p className='text-right leading-tight'>
@@ -27,18 +32,22 @@ export default function HomeHeader({ mainProduct }: { mainProduct: Product }) {
 			<div className='flex flex-wrap basis-1/2 justify-center items-center px-10 py-4'>
 				<Image
 					className='w-auto h-auto object-contain'
-					alt={title}
-					src={`${baseImgUrl}${images[0]}`}
+					alt={mainProduct.title}
+					src={`${baseImgUrl}${mainProduct.images[0]}`}
 					width='0'
 					height='0'
 					sizes='100vw'
 				/>
 				<div className='text-center mr-3'>
-					<h2>{title}</h2>
-					<p>&#8364;{price}</p>
+					<h2>{mainProduct.title}</h2>
+					<p>&#8364;{mainProduct.price}</p>
 				</div>
 				<Button textContent='Details' />
 			</div>
+
+			{products.map((product: ProductProps) => (
+				<Product key={product._id} product={product} />
+			))}
 		</section>
 	);
 }

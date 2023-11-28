7ae5d2da0cb832961e0232e6fbd0cbd0793edb9f
GrzegorzWirtek <grzegorz.wirtek@wp.pl>
2023-11-28 17:54:20 +0100

    replace '%20' with a space in url search param

--- app/(pages)/search/[search]/page.tsx
+++ app/(pages)/search/[search]/page.tsx
@@ -6,18 +6,20 @@ import NoResults from '@/components/no-results';
 export default async function Search({ params }: SearchParams) {
 	const limit = parseInt(process.env.NEXT_PUBLIC_PAGES_LIMIT!);
 
+	const search = params.search.replace(/%20/g, ' ');
+
 	const products = await getProducts({
-		searchByPhraze: { search: params.search },
+		searchByPhraze: { search },
 	});
 
-	if (!products || !products.length) return <NoResults value={params.search} />;
+	if (!products || !products.length) return <NoResults value={search} />;
 
 	return (
 		<div className='flex flex-wrap lg:flex-nowrap justify-center lg:px-10 pb-10'>
 			<ProductsList
 				products={products}
 				limit={limit}
-				searchByPhraze={{ search: params.search }}
+				searchByPhraze={{ search }}
 			/>
 		</div>
 	);
